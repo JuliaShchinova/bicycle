bccf77e6882d0e1c2c16493ec727755a4bfa195f
Julia <sablina83@yandex.ru>
2020-11-06 16:10:57 +0300

    Итоговый вариант

--- source/js/script.js
+++ source/js/script.js
@@ -1,20 +1,20 @@
 'use strict';
 
 (function () {
-var pageHeader = document.querySelector('.page-header');
-var mainNavToggle = pageHeader.querySelector('.main-nav__toggle');
+  var pageHeader = document.querySelector('.page-header');
+  var mainNavToggle = pageHeader.querySelector('.main-nav__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+  pageHeader.classList.remove('page-header--nojs');
 
-mainNavToggle.addEventListener('click', function () {
-  pageHeader.classList.toggle('page-header--open');
+  mainNavToggle.addEventListener('click', function () {
+    pageHeader.classList.toggle('page-header--open');
 
-  if (pageHeader.classList.contains('page-header--open')) {
-    mainNavToggle.classList.add('main-nav__toggle--close');
-    mainNavToggle.classList.remove('main-nav__toggle--open');
-  } else {
-    mainNavToggle.classList.add('main-nav__toggle--open');
-    mainNavToggle.classList.remove('main-nav__toggle--close');
-  }
-});
+    if (pageHeader.classList.contains('page-header--open')) {
+      mainNavToggle.classList.add('main-nav__toggle--close');
+      mainNavToggle.classList.remove('main-nav__toggle--open');
+    } else {
+      mainNavToggle.classList.add('main-nav__toggle--open');
+      mainNavToggle.classList.remove('main-nav__toggle--close');
+    }
+  });
 })();
